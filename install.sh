47467306a3a3d3058f26d6c98325f96c1b699149
Renee196 <76276943+Renee196@users.noreply.github.com>
2026-03-16 20:44:55 +0900

    Add installation steps for ROS Noetic and dependencies

--- install.sh
+++ install.sh
@@ -100,10 +100,121 @@ echo "================================="
 echo "ISAACLAB INSTALL COMPLETE"
 echo "================================="
 
+########################################
+# 9 Install ROS Noetic
+########################################
+
+echo "Installing ROS Noetic..."
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
+sudo apt update
+
+sudo apt install -y ros-noetic-desktop-full
+
+########################################
+# 10 Setup ROS environment
+########################################
+
+echo "Setting up ROS environment..."
+
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+
+source /opt/ros/noetic/setup.bash
+
+########################################
+# 11 Install ROS build tools
+########################################
+
+sudo apt install -y \
+    python3-rosdep \
+    python3-rosinstall \
+    python3-rosinstall-generator \
+    python3-wstool \
+    python3-catkin-tools
+
+sudo rosdep init || true
+rosdep update
+
+
+########################################
+# 14 Adding Eigen, RBDL, qpOASES
+########################################
+
+sudo apt install -y \
+    libeigen3-dev \
+    libyaml-cpp-dev \
+    libboost-all-dev
+
+    ########################################
+# Install RBDL
+########################################
+
+echo "Installing RBDL..."
+
+cd ~
+
+git clone https://github.com/rbdl/rbdl.git
+
+cd rbdl
+
+mkdir build
+cd build
+
+cmake ..
+
+make -j$(nproc)
+
+sudo make install
+
+########################################
+# Install qpOASES
+########################################
+
+echo "Installing qpOASES..."
+
+cd ~
+
+git clone https://github.com/coin-or/qpOASES.git
+
+cd qpOASES
+
+mkdir build
+cd build
+
+cmake ..
+
+make -j$(nproc)
+
+sudo make install
+
+########################################
+# 11 Install TOCABI
+########################################
+
 echo "Downloading TOCABI packages..."
 
 wget --no-check-certificate "https://drive.google.com/uc?export=download&id=1yNfBKNSicUEOmNNVmM9JLR-eMDfcHoqH" -O dyros_tocabi_v2.zip
+pip install gdown
+gdown 1yNfBKNSicUEOmNNVmM9JLR-eMDfcHoqH
+unzip dyros_tocabi_v2.zip
 
 echo "Extracting TOCABI packages..."
 
 unzip dyros_tocabi_v2.zip
+
+########################################
+# 14 Create ROS workspace
+########################################
+
+echo "Creating TOCABI workspace..."
+
+mkdir -p ~/tocabi_ws/src
+
+cd ~/tocabi_ws
+
+catkin_make
+
+echo "source ~/tocabi_ws/devel/setup.bash" >> ~/.bashrc
